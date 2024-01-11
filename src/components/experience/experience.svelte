7540ffede6bdd2a8a97dc4c8e616bd5c5403deb7
AdiChops <aaditya.chopra@carleton.ca>
2024-01-11 02:12:08 -0500

    styling changes

--- src/components/experience/experience.svelte
+++ src/components/experience/experience.svelte
@@ -62,40 +62,41 @@
 
 </script>
 
-<p class="heading">Work Experiences</p>
-<div id="companies">
-    {#each experiences as experience}
-        <div class="company-badge" id="exp-{experience.company.toString().replace(/ /g, '')}" style="--bg-image:url({experience.bgImage}); --bg-color:{experience.bgColor};"></div>
-    {/each}
+<div id="experience">
+    <p class="heading">Work Experiences</p>
+    <div id="companies">
+        {#each experiences as experience}
+            <div class="company-badge" id="exp-{experience.company.toString().replace(/ /g, '')}" style="--bg-image:url({experience.bgImage}); --bg-color:{experience.bgColor};"></div>
+        {/each}
+    </div>
+    <div class="timeline">
+        <!-- Code adapted from Mark Boots (CodePen) - Adapted from this codepen: https://codepen.io/MarkBoots/pen/OJOqNyB -->
+        <ul>
+        {#each experiences as experience}
+            <li class="exp" data-experience="exp-{experience.company.toString().replace(/ /g, '')}" style="--accent-color:{experience.accentColor}; --bg-image:url({experience.bgImage}); --bg-color:{experience.bgColor}">
+                <div class="date">{experience.duration}</div>
+                <div class="title">{experience.position} @ {experience.company}</div>
+                <div class="descr">{experience.description}</div>
+                {#if experience.technologies}
+                    <div class="technologies">
+                        <p>Technologies:</p>
+                            {#each experience.technologies as technology}
+                            <div class="tech">
+                                {#if technology != "Mockito"}
+                                <Icon class="icon" icon={icons[technology]} />
+                                {:else}
+                                <img class="icon" width="40" height="40" src="/images/mockito.jpg" alt="Mockito logo">
+                                {/if}
+                                <span class="subtitle">{technology}</span>
+                            </div>
+                            {/each}
+                    </div>
+                {/if}
+            </li>
+        {/each}
+        </ul>
+    </div>
 </div>
-<div class="timeline">
-    <!-- Code adapted from Mark Boots (CodePen) - Adapted from this codepen: https://codepen.io/MarkBoots/pen/OJOqNyB -->
-    <ul>
-    {#each experiences as experience}
-        <li class="exp" data-experience="exp-{experience.company.toString().replace(/ /g, '')}" style="--accent-color:{experience.accentColor}; --bg-image:url({experience.bgImage}); --bg-color:{experience.bgColor}">
-            <div class="date">{experience.duration}</div>
-            <div class="title">{experience.position} @ {experience.company}</div>
-            <div class="descr">{experience.description}</div>
-            {#if experience.technologies}
-                <div class="technologies">
-                    <p>Technologies:</p>
-                        {#each experience.technologies as technology}
-                        <div class="tech">
-                            {#if technology != "Mockito"}
-                            <Icon class="icon" icon={icons[technology]} />
-                            {:else}
-                            <img class="icon" width="40" height="40" src="/images/mockito.jpg" alt="Mockito logo">
-                            {/if}
-                            <span class="subtitle">{technology}</span>
-                        </div>
-                        {/each}
-                </div>
-            {/if}
-        </li>
-    {/each}
-    </ul>
-</div>
-
 <style>
     @import './experience.css';
 </style>
